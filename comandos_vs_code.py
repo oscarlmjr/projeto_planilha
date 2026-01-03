f74ef945408ca260fe92071019985fb3c9227dd0
oscarlmjr <oscarlmjr@gmail.com>
2026-01-02 21:16:42 -0300

    executando extração de imagem

---
+++ comandos_vs_code.py
@@ -0,0 +1,52 @@
+# Para mostrar caracteres de tabulação (Tab) e espaços no VS Code, ative a opção "Render Whitespace" (Renderizar Espaço em Branco) para ver setas (Tab) e pontos (espaços) continuamente, usando o atalho Ctrl+Shift+P e buscando por Render Whitespace, ou clicando no indicador "SPC" ou "TAB" na barra de status inferior para alternar a visualização.
+
+# Para usar caracteres de tabulação (tab characters):
+# Desmarque a caixa de seleção em Editor: Insert Spaces.
+# A tecla Tab passará a inserir um caractere de tabulação real.
+
+# Para quebrar linha no VS Code, você pode usar o atalho Alt + Z para ativar/desativar a quebra de linha "virtual" (apenas visual) ou ir em Arquivo > Preferências > Configurações (ou Ctrl+,), pesquisar por editor.wordWrap e selecionar "on" para uma quebra automática por padrão, que ajusta o texto à largura da janela sem adicionar caracteres de nova linha no arquivo.
+
+# multiplica cursor: Ctrl + Alt + arrow
+# multiplica cursor embaixo do cursor original: Shift + Alt + click 
+# multiplica cursor: Alt + click 
+# Move line up/down: Alt + ↑ / ↓ 
+# Desloca código para o sentido da seta: Ctrl + Shift + seta  
+# Duplica a seleção: Alt + s
+# Pesquisar: Ctrl + f
+# Comenta linha: Ctrl + Alt + z
+# Remove comentários: Ctrl + Alt + a
+# Enter no commit: Ctrl + Enter
+# Adiconar cursor à seleção: Ctrl + Shift + l
+# Replace: Ctrl + h
+# Emoji: Windows + .
+# sessão de API web, como FastAPI, use Ctrl+C para enviar um sinal de interrupção ao processo.
+
+# Para "encerrar" uma sessão de API no PowerShell, o método correto depende do con
+# texto: para sair de uma sessão interativa do PowerShell, use Exit-PSSession ou a
+# palavra-chave exit; para parar um processo que está executando uma API, use 
+# Stop-Process.
+
+# ________
+
+# 22'							# 5 16'
+# 1 51'							# 4 59'
+# 11 30'						# 9 4'
+# 13 31'						# 2 36'
+# 11 18'						# 11 1'
+# 10 53' --> *48 24'			# 11 4'
+# *4h 40min						# 2 52'
+# -------						# 1	'
+# 53º40'						# 5 29'
+								# 12 23'
+								# 4 6'
+								# 1'
+								# 2 --> -65h 45min
+								# -16'53"
+								# ------
+								# -82º30'
+								# 2 --> -65h 45min
+								# -16'53"
+								# ------
+								# -82º30'
+
+#--------------------------------------------------
--- main.py
+++ main.py
@@ -3,7 +3,7 @@ from PyPDF2 import PdfReader
 
 
 PASTA_RAIZ = Path(__file__).parent
-PASTA_EXTRATO = PASTA_RAIZ / 'pdfs_extrato'
+PASTA_EXTRATO = PASTA_RAIZ / 'pdfs_extrato' / '2025' / '11.2025'
 PASTA_NOVA = PASTA_RAIZ / 'extratos_novos'
 
 EXTRATO_BANCARIO = PASTA_EXTRATO / 'Banco do Brasil.pdf'
@@ -11,3 +11,22 @@ EXTRATO_BANCARIO = PASTA_EXTRATO / 'Banco do Brasil.pdf'
 PASTA_NOVA.mkdir(exist_ok=True)
 
 
+reader = PdfReader(EXTRATO_BANCARIO)
+
+page0 = reader.pages[0]
+imagem0 = page0.images[0]
+
+with open(PASTA_NOVA / imagem0.name, 'wb') as fp:
+    fp.write(imagem0.data)
+
+# with open(PASTA_NOVA / page0.images[0].name, 'wb') as imagem:
+    # imagem.writte()
+
+# print(page0.images[0])
+# print(len(page0.images))
+# print(page0.images)
+# print(page0.extract_text())
+# print(len(reader.pages))
+
+# for page in reader.pages:
+#     print(page)
