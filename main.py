59381eda0870042ffc189f4061ad550b6aa08c97
oscarlmjr <oscarlmjr@gmail.com>
2026-01-03 20:02:06 -0300

    adicionando arquivos .csv

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 from pathlib import Path
-from PyPDF2 import PdfReader
+from PyPDF2 import PdfReader, PdfWriter, PdfMerger
 
 
 PASTA_RAIZ = Path(__file__).parent
@@ -14,10 +14,47 @@ PASTA_NOVA.mkdir(exist_ok=True)
 reader = PdfReader(EXTRATO_BANCARIO)
 
 page0 = reader.pages[0]
-imagem0 = page0.images[0]
+# imagem0 = page0.images[0]
 
-with open(PASTA_NOVA / imagem0.name, 'wb') as fp:
-    fp.write(imagem0.data)
+for i, page in enumerate(reader.pages):
+    writer = PdfWriter()
+    with open(PASTA_NOVA / f'page{i}.pdf', 'wb') as arquivo:
+        writer.add_page(page)
+        writer.write(arquivo)
+
+
+files = [
+    PASTA_NOVA / 'page1.pdf',
+    PASTA_NOVA / 'page0.pdf',
+]
+
+merger = PdfMerger()
+for file in files:
+    merger.append(file)
+    
+merger.write(PASTA_NOVA / 'MERGED.pdf')
+merger.close()
+
+# for i, page in enumerate(reader.pages):
+#     writer = PdfWriter()
+#     with open(PASTA_NOVA / f'page{i}.pdf', 'wb') as arquivo:
+#         writer.add_page(page)
+#         writer.write(arquivo)
+
+# writer = PdfWriter()
+
+# with open(PASTA_NOVA / 'NOVO_PDF.pdf', 'wb') as arquivo:
+# 	for page in reader.pages:
+# 		writer.add_page(page0)
+# 	writer.write(arquivo)
+
+# writer.add_page(page0)
+
+# with open(PASTA_NOVA / 'page0.pdf', 'wb') as arquivo:
+# 	writer.write(arquivo)
+
+# with open(PASTA_NOVA / imagem0.name, 'wb') as fp:
+#     fp.write(imagem0.data)
 
 # with open(PASTA_NOVA / page0.images[0].name, 'wb') as imagem:
     # imagem.writte()
